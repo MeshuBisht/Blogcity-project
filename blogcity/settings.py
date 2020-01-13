570cd9225c57746f531f966f7fdcbb3dbe9304c5
1m3bisht@gmail.com <amitbisht9968@gmail.com>
2020-01-13 21:17:51 +0530

    in terminal

--- blogcity/settings.py
+++ blogcity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$*mg(gn(#g9_3jr%wc*as+txs3mu3pi4!rp7_h66sgternk*r@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
